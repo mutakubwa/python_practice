a4531cf268462877bb60a853900fb8220dd45da8
John Mutakubwa <mutakubwa@gmail.com>
2021-04-25 15:53:01 -0400

    missing number algo changes

--- missingnumber.py
+++ missingnumber.py
@@ -10,20 +10,32 @@ def missing_number_1(size_num, list_num):
 
     return missing
 
+def missing_number_2(size_num, list_num):
+    missing = None
+    set_size = set(range(1, size_num+1))
+    set_num = set(list_num)
+    found = set_size ^ set_num
+    missing = found.pop()
+
+    return missing
+
 #get number to get size
 while(True):
     size_num = int(input())
-    if (size_num > 1):
+    if (size_num > 1 and size_num <= (2*10**5)):
         break
-
-
 
 #get list of numbers to search in and make list
 entry = input().split(' ')
-list_num = [int(x) for x in list(entry)]
-list_num.sort()
-#get missing number
-print(missing_number_1(size_num, list_num))
+#covert to int with comprehesion
+#list_num = [int(x) for x in list(entry)]
+#convert to int with map
+list_num = list(map(int, entry))
+#list_num.sort()
+#get missing number wuth method 1 - linear operation
+#print(missing_number_1(size_num, list_num))
+#get missing number with method 2 sets
+print(missing_number_2(size_num, list_num))
 
 
 
--- missingnumber_test.py
+++ missingnumber_test.py
@@ -1,5 +1,5 @@
 import unittest 
-from missingnumber import missing_number_1
+from missingnumber import missing_number_1, missing_number_2
 
 class MissingNumber_Test(unittest.TestCase):
     def test_missingnumber1(self):
@@ -19,6 +19,24 @@ class MissingNumber_Test(unittest.TestCase):
         testcase2 = [5,5,3,2,4]
         expected = 1
         self.assertEqual(missing_number_1(testcase1, testcase2), expected)
+    
+    def test_missingnumber4(self):
+        testcase1 = 5
+        testcase2 = [2,2,3,4,5]
+        expected = 1
+        self.assertEqual(missing_number_2(testcase1, testcase2), expected)
+
+    def test_missingnumber5(self):
+        testcase1 = 5
+        testcase2 = [2,2,5,4,1]
+        expected = 3
+        self.assertEqual(missing_number_2(testcase1, testcase2), expected)
+
+    def test_missingnumber6(self):
+        testcase1 = 5
+        testcase2 = [5,5,3,2,4]
+        expected = 1
+        self.assertEqual(missing_number_2(testcase1, testcase2), expected)
 
 if __name__ == '__main__':
     unittest.main()
